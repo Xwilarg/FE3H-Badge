132059dd7820b705340717020abee032276a2461
Xwilarg <xwilarg@yahoo.fr>
2019-08-10 12:48:17 +0200

    Add base architecture

---
+++ js/main.js
@@ -0,0 +1,14 @@
+class Character {
+    constructor(image, houseName) {
+        this.image = image;
+        this.houseName = houseName;
+    }
+}
+
+let allCharacters = new Map([
+    ["Bernadetta", new Character("img/Bernadetta.png", "Blue Lions")]
+])
+
+function process() {
+    
+}
