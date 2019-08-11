fe99024e0a1847d3f069b8a43e5cdd1a77288505
Xwilarg <xwilarg@yahoo.fr>
2019-08-11 03:03:25 +0200

    Add characters

--- js/main.js
+++ js/main.js
@@ -1,12 +1,45 @@
 class Character {
-    constructor(image, houseName) {
-        this.image = image;
+    constructor(houseName) {
         this.houseName = houseName;
     }
 }
 
 let allCharacters = new Map([
-    ["Bernadetta", new Character("img/Bernadetta.png", "Blue Lions")]
+    ["Edelgard", new Character("Black Eagles")],
+    ["Hubert", new Character("Black Eagles")],
+    ["Dorothea", new Character("Black Eagles")],
+    ["Ferdinand", new Character("Black Eagles")],
+    ["Bernadetta", new Character("Black Eagles")],
+    ["Caspar", new Character("Black Eagles")],
+    ["Petra", new Character("Black Eagles")],
+    ["Linhardt", new Character("Black Eagles")],
+    ["Dimitri", new Character("Blue Lions")],
+    ["Dedue", new Character("Blue Lions")],
+    ["Felix", new Character("Blue Lions")],
+    ["Mercedes", new Character("Blue Lions")],
+    ["Ashe", new Character("Blue Lions")],
+    ["Annette", new Character("Blue Lions")],
+    ["Sylvain", new Character("Blue Lions")],
+    ["Ingrid", new Character("Blue Lions")],
+    ["Claude", new Character("Golden Deer")],
+    ["Hilda", new Character("Golden Deer")],
+    ["Lorenz", new Character("Golden Deer")],
+    ["Raphael", new Character("Golden Deer")],
+    ["Lysethia", new Character("Golden Deer")],
+    ["Ignatz", new Character("Golden Deer")],
+    ["Marianne", new Character("Golden Deer")],
+    ["Leonie", new Character("Golden Deer")],
+    ["Manuela", new Character("Others")],
+    ["Hanneman", new Character("Others")],
+    ["Catherine", new Character("Others")],
+    ["Alois", new Character("Others")],
+    ["Seteth", new Character("Others")],
+    ["Flayn", new Character("Others")],
+    ["Gilbert", new Character("Others")],
+    ["Shamir", new Character("Others")],
+    ["Cyril", new Character("Others")],
+    ["Rhea", new Character("Others")],
+    ["Sothis", new Character("Others")]
 ])
 
 function process() {
