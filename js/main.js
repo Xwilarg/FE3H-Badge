ea21c51da9bc06b695d66e1345422779a0e9ef24
Xwilarg <xwilarg@yahoo.fr>
2019-08-11 12:45:18 +0200

    Display characters on canvas

--- js/main.js
+++ js/main.js
@@ -1,51 +1,60 @@
 class Character {
-    constructor(houseName) {
+    constructor(name, houseName) {
+        this.name = name;
         this.houseName = houseName;
     }
 }
 
 let allCharacters = new Map([
-    ["Edelgard", new Character("Black Eagles")],
-    ["Hubert", new Character("Black Eagles")],
-    ["Dorothea", new Character("Black Eagles")],
-    ["Ferdinand", new Character("Black Eagles")],
-    ["Bernadetta", new Character("Black Eagles")],
-    ["Caspar", new Character("Black Eagles")],
-    ["Petra", new Character("Black Eagles")],
-    ["Linhardt", new Character("Black Eagles")],
-    ["Dimitri", new Character("Blue Lions")],
-    ["Dedue", new Character("Blue Lions")],
-    ["Felix", new Character("Blue Lions")],
-    ["Mercedes", new Character("Blue Lions")],
-    ["Ashe", new Character("Blue Lions")],
-    ["Annette", new Character("Blue Lions")],
-    ["Sylvain", new Character("Blue Lions")],
-    ["Ingrid", new Character("Blue Lions")],
-    ["Claude", new Character("Golden Deer")],
-    ["Hilda", new Character("Golden Deer")],
-    ["Lorenz", new Character("Golden Deer")],
-    ["Raphael", new Character("Golden Deer")],
-    ["Lysethia", new Character("Golden Deer")],
-    ["Ignatz", new Character("Golden Deer")],
-    ["Marianne", new Character("Golden Deer")],
-    ["Leonie", new Character("Golden Deer")],
-    ["Manuela", new Character("Others")],
-    ["Hanneman", new Character("Others")],
-    ["Catherine", new Character("Others")],
-    ["Alois", new Character("Others")],
-    ["Seteth", new Character("Others")],
-    ["Flayn", new Character("Others")],
-    ["Gilbert", new Character("Others")],
-    ["Shamir", new Character("Others")],
-    ["Cyril", new Character("Others")],
-    ["Rhea", new Character("Others")],
-    ["Sothis", new Character("Others")]
+    ["Edelgard", new Character("Edelgard", "Black Eagles")],
+    ["Hubert", new Character("Hubert", "Black Eagles")],
+    ["Dorothea", new Character("Dorothea", "Black Eagles")],
+    ["Ferdinand", new Character("Ferdinand", "Black Eagles")],
+    ["Bernadetta", new Character("Bernadetta", "Black Eagles")],
+    ["Caspar", new Character("Caspar", "Black Eagles")],
+    ["Petra", new Character("Petra", "Black Eagles")],
+    ["Linhardt", new Character("Linhardt", "Black Eagles")],
+    ["Dimitri", new Character("Dimitri", "Blue Lions")],
+    ["Dedue", new Character("Dedue", "Blue Lions")],
+    ["Felix", new Character("Felix", "Blue Lions")],
+    ["Mercedes", new Character("Mercedes", "Blue Lions")],
+    ["Ashe", new Character("Ashe", "Blue Lions")],
+    ["Annette", new Character("Annette", "Blue Lions")],
+    ["Sylvain", new Character("Sylvain", "Blue Lions")],
+    ["Ingrid", new Character("Ingrid", "Blue Lions")],
+    ["Claude", new Character("Claude", "Golden Deer")],
+    ["Hilda", new Character("Hilda", "Golden Deer")],
+    ["Lorenz", new Character("Lorenz", "Golden Deer")],
+    ["Raphael", new Character("Raphael", "Golden Deer")],
+    ["Lysithea", new Character("Lysithea", "Golden Deer")],
+    ["Ignatz", new Character("Ignatz", "Golden Deer")],
+    ["Marianne", new Character("Marianne", "Golden Deer")],
+    ["Leonie", new Character("Leonie", "Golden Deer")],
+    ["Manuela", new Character("Manuela", "Others")],
+    ["Hanneman", new Character("Hanneman", "Others")],
+    ["Catherine", new Character("Catherine", "Others")],
+    ["Alois", new Character("Alois", "Others")],
+    ["Seteth", new Character("Seteth", "Others")],
+    ["Flayn", new Character("Flayn", "Others")],
+    ["Gilbert", new Character("Gilbert", "Others")],
+    ["Shamir", new Character("Shamir", "Others")],
+    ["Cyril", new Character("Cyril", "Others")],
+    ["Rhea", new Character("Rhea", "Others")],
+    ["Sothis", new Character("Sothis", "Others")]
 ])
 
 function process() {
     let ctx = document.getElementById('canvas').getContext('2d');
-    loadImage(ctx, "img/Bernadetta.png", 0, 0);
-    loadImage(ctx, "img/Mask.png", 0, 0);
+    let i = 0;
+    allCharacters.forEach(function(e) {
+        if (document.getElementById(e.name).checked) {
+            loadImage(ctx, "img/" + e.name + ".png", (i % 8) * 130, Math.floor(i / 8) * 130);
+            loadImage(ctx, "img/Mask.png", (i % 8) * 130, Math.floor(i / 8) * 130);
+        } else {
+            loadImage(ctx, "img/Empty.png", (i % 8) * 130, Math.floor(i / 8) * 130);
+        }
+        i++;
+    });
 }
 
 function loadImage(ctx, path, x, y) {
